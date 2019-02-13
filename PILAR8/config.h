81ec3c7538753fbb219e70ff09d9f5625418726d
Jose R Valverde <jrvalverde@cnb.csic.es>
2019-02-13 11:44:40 +0100

    Add files via upload

---
+++ PILAR8/config.h
@@ -0,0 +1,39 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you don't have vprintf but do have _doprnt.  */
+/* #undef HAVE_DOPRNT */
+
+/* Define if you have the vprintf function.  */
+#define HAVE_VPRINTF 1
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if the X Window System is missing or not being used.  */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the <dlfcn.h> header file.  */
+#define HAVE_DLFCN_H 1
+
+/* Define if you have the <include/P_global.h> header file.  */
+/* #undef HAVE_INCLUDE_P_GLOBAL_H */
+
+/* Define if you have the <malloc.h> header file.  */
+#define HAVE_MALLOC_H 1
+
+/* Name of package */
+#define PACKAGE "pilar"
+
+/* Version number of package */
+#define VERSION "8.0"
+
